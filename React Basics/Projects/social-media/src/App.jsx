047f2c97f0f63fe7264609cbfc96d1cff5f3a059
Rajkumar9898 <razchaurasia5@gmail.com>
2025-08-19 20:36:38 -0400

    structure done

--- React Basics/Projects/social-media/src/App.jsx
+++ React Basics/Projects/social-media/src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SideBar from "./components/SideBar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
 
 function App() {
   return (
-    <>
-    <Header></Header>    
-    </>
+    <div className="app-container">
+      <SideBar></SideBar>
+      <div className="content">
+      <Header></Header>
+      <CreatePost></CreatePost>
+      <PostList></PostList>
+      <Footer></Footer>
+      </div>
+    </div>
   );
 }
 
